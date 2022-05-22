c8dc7b190a5f547ab749a532f8ae495df285e027
Allysson <allyssonmgs@gmail.com>
2022-05-22 17:26:17 -0300

    feat: ensure SignUpUsecase returns SignUpError when LoadUserRepository returns user data

--- src/data/usecases/sign-up.ts
+++ src/data/usecases/sign-up.ts
@@ -1,17 +1,19 @@
 import { SignUp } from '@/domain/usecases';
+import { SignUpError } from '@/domain/errors';
 import { Hasher } from '@/data/protocols';
 import { LoadUserRepository } from '@/data/protocols/repositories';
 
-export class SignUpUsecase {
+export class SignUpUsecase implements SignUp {
   constructor(
     private readonly hasher: Hasher,
     private readonly loadUserRepository: LoadUserRepository,
   ) {}
 
-  async perform(params: SignUp.Params): Promise<void> {
+  async perform(params: SignUp.Params): Promise<SignUp.Result> {
     const { email, password } = params;
-
     await this.hasher.hash({ plaintext: password });
     await this.loadUserRepository.load({ email });
+
+    return new SignUpError();
   }
 }
