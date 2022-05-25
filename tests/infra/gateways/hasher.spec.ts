c762bae725bfc035125ed8ef1d16d65b95754223
Allysson <allyssonmgs@gmail.com>
2022-05-25 20:52:09 -0300

    test: ensure BcryptAdapter hash returns a hashed value on success

--- tests/infra/gateways/hasher.spec.ts
+++ tests/infra/gateways/hasher.spec.ts
@@ -2,6 +2,12 @@ import bcrypt from 'bcrypt';
 
 import { BcryptAdapter } from '@/infra/gateways/bcrypt-adapter';
 
+jest.mock('bcrypt', () => ({
+  async hash(): Promise<string> {
+    return Promise.resolve('hashed_string');
+  },
+}));
+
 describe('Hasher', () => {
   it('Should call hash with correct params', async () => {
     const salt = 12;
@@ -9,5 +15,12 @@ describe('Hasher', () => {
     const hashSpy = jest.spyOn(bcrypt, 'hash');
     await sut.hash({ plaintext: 'any_string' });
     expect(hashSpy).toHaveBeenCalledWith('any_string', salt);
+  });
+
+  it('Should return a result on succes', async () => {
+    const salt = 12;
+    const sut = new BcryptAdapter(salt);
+    const result = await sut.hash({ plaintext: 'any_string' });
+    expect(result).toEqual({ ciphertext: 'hashed_string' });
   });
 });
