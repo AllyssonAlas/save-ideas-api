df35b7dcecded5c13ecdea7bbe5079196371e7e8
Allysson <allyssonmgs@gmail.com>
2022-06-18 22:06:19 -0300

    refactor: move sut creation to a factory helper method

--- tests/infra/db/firestore/user-repository.spec.ts
+++ tests/infra/db/firestore/user-repository.spec.ts
@@ -1,5 +1,9 @@
 import { FirestoreHelper, UserRepository } from '@/infra/db';
 
+const makeSut = (): UserRepository => {
+  return new UserRepository();
+};
+
 describe('UsersRepository', () => {
   beforeAll(() => {
     FirestoreHelper.connect();
@@ -11,7 +15,8 @@ describe('UsersRepository', () => {
 
   describe('add()', () => {
     test('Should return an user name and email on success', async () => {
-      const sut = new UserRepository();
+      const sut = makeSut();
+
       const user = await sut.create({
         name: 'any_name',
         email: 'any_email@example.com',
