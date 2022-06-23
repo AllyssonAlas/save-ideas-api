9ff7335d68c0ea3ff3122c288ab7f6308bffa402
Allysson <allyssonmgs@gmail.com>
2022-06-22 21:58:48 -0300

    feat: ensure SignUpController calls SignUpUsecase with correct values once

--- src/presentation/controllers/sign-up-controller.ts
+++ src/presentation/controllers/sign-up-controller.ts
@@ -1,22 +1,24 @@
+import { SignUp } from '@/domain/usecases';
 import { HttpRequest, HttpResponse, Controller, EmailValidator } from '@/presentation/protocols';
 import { MissingParamError, InvalidParamError } from '@/presentation/errors';
 import { badRequest, serverError } from '@/presentation/helpers';
 
 export class SignUpController implements Controller {
-  constructor(private readonly emailValidator: EmailValidator) {}
+  constructor(private readonly emailValidator: EmailValidator, private readonly signUp: SignUp) {}
 
-  handle(httpRequest: HttpRequest): HttpResponse | any {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse | any> {
     try {
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) return badRequest(new MissingParamError(field));
       }
-      const { password, passwordConfirmation, email } = httpRequest.body;
+      const { name, email, password, passwordConfirmation } = httpRequest.body;
       if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'));
       }
       const isEmailValid = this.emailValidator.isValid(email);
       if (!isEmailValid) return badRequest(new InvalidParamError('email'));
+      await this.signUp.perform({ name, email, password });
     } catch (error) {
       return serverError();
     }
