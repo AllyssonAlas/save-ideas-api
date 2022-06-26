be890d2b4f0567b1dba622c202bdb449ad17e290
Allysson <allyssonmgs@gmail.com>
2022-06-25 21:27:11 -0300

    feat: ensure SignUp route returns an user on success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import setupMiddlewares from '@/main/config/middlewares';
+import setupRoutes from '@/main/config/routes';
 
 const app = express();
 
 setupMiddlewares(app);
+setupRoutes(app);
 
 export default app;
---
+++ src/main/config/routes.ts
@@ -0,0 +1,14 @@
+import { Express, Router } from 'express';
+import { readdirSync } from 'fs';
+
+export default (app: Express): void => {
+  const router = Router();
+
+  app.use('/api', router);
+
+  readdirSync(`${String(__dirname)}/../routes`).map(async (file) => {
+    if (!file.endsWith('.map') && !file.includes('.test.')) {
+      (await import(`../routes/${file}`)).default(router);
+    }
+  });
+};
