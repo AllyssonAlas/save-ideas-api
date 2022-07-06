85cfcdf335201d93730fd50820f0068b1a2204a0
Allysson <allyssonmgs@gmail.com>
2022-07-05 21:56:11 -0300

    refactor: change Validation return strategy

--- tests/presentation/mocks/mock-validation.ts
+++ tests/presentation/mocks/mock-validation.ts
@@ -1,11 +1,11 @@
 import { Validation } from '@/presentation/protocols';
 
 export class ValidationSpy implements Validation {
-  error: Error | null = null;
+  error: Error | undefined = undefined;
   callsCount = 0;
   input: any;
 
-  validate(input: any): Error | null {
+  validate(input: any): Error | undefined {
     this.input = input;
     this.callsCount++;
     return this.error;
