e50b0e87ba3c89513718a0996cf4d83ecf05b0fd
Allysson <allyssonmgs@gmail.com>
2022-07-09 15:46:56 -0300

    test: ensure ValidationComposite returns the first error if more than one validation fails

--- tests/presentation/validators/validation-composite.spec.ts
+++ tests/presentation/validators/validation-composite.spec.ts
@@ -5,22 +5,32 @@ import { ValidationSpy } from '@/tests/presentation/mocks';
 
 interface SutTypes {
   sut: ValidationComposite;
-  validationSpy: ValidationSpy;
+  validationSpies: ValidationSpy[];
 }
 
 const makeSut = (): SutTypes => {
-  const validationSpy = new ValidationSpy();
-  const sut = new ValidationComposite([validationSpy]);
-  return { sut, validationSpy };
+  const validationSpies = [new ValidationSpy(), new ValidationSpy()];
+  const sut = new ValidationComposite(validationSpies);
+  return { sut, validationSpies };
 };
 
 describe('ValidationComposite', () => {
   test('Should return an error if any validation fails', () => {
-    const { sut, validationSpy } = makeSut();
-    validationSpy.error = new MissingParamError('field');
+    const { sut, validationSpies } = makeSut();
+    validationSpies[0].error = new MissingParamError('field');
 
     const error = sut.validate({ field: 'any_value' });
 
     expect(error).toEqual(new MissingParamError('field'));
+  });
+
+  test('Should return the first error if more than one validation fails', () => {
+    const { sut, validationSpies } = makeSut();
+    validationSpies[0].error = new Error();
+    validationSpies[1].error = new MissingParamError('field');
+
+    const error = sut.validate({ field: 'any_value' });
+
+    expect(error).toEqual(new Error());
   });
 });
