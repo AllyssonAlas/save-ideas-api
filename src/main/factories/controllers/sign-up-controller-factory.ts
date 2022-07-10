6933b3d7d0d26f657d476935b9f9e6348f673e3a
Allysson <allyssonmgs@gmail.com>
2022-07-10 17:56:29 -0300

    feat: add LogRepository to makeSignUpController

--- src/main/factories/controllers/sign-up-controller-factory.ts
+++ src/main/factories/controllers/sign-up-controller-factory.ts
@@ -3,8 +3,10 @@ import { makeSignUpUseCase } from '@/main/factories/usecases';
 import { makeSignUpValidation } from '@/main/factories/validations';
 import { Controller } from '@/presentation/protocols';
 import { LogControllerDecorator } from '@/main/decorators/log';
+import { LogRepository } from '@/infra/db';
 
 export const makeSignUpController = (): Controller => {
+  const logRepository = new LogRepository();
   const signUpController = new SignUpController(makeSignUpValidation(), makeSignUpUseCase());
-  return new LogControllerDecorator(signUpController);
+  return new LogControllerDecorator(signUpController, logRepository);
 };
