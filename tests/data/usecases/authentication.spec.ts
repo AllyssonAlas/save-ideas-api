39fd9aa9fe571498ffc3bb31602bb454fd329dab
AllyssonAlas <allysson.alas@fabricainfo.com>
2022-07-11 16:22:26 -0300

    refactor: move sut creation to a factory method

--- tests/data/usecases/authentication.spec.ts
+++ tests/data/usecases/authentication.spec.ts
@@ -2,16 +2,27 @@ import { AuthenticationUsecase } from '@/data/usecases';
 
 import { LoadUserRepositorySpy } from '@/tests/data/mocks';
 
+interface SutTypes {
+  sut: AuthenticationUsecase;
+  loadUserRepositorySpy: LoadUserRepositorySpy;
+}
+
+const makeSut = (): SutTypes => {
+  const loadUserRepositorySpy = new LoadUserRepositorySpy();
+  const sut = new AuthenticationUsecase(loadUserRepositorySpy);
+
+  return { sut, loadUserRepositorySpy };
+};
+
 describe('AuthenticationUsecase', () => {
-  test('Should call LoadUserRepository with correct value', () => {
-    const loadUserRepositorySpy = new LoadUserRepositorySpy();
-    const sut = new AuthenticationUsecase(loadUserRepositorySpy);
+  test('Should call LoadUserRepository with correct value', async () => {
+    const { sut, loadUserRepositorySpy } = makeSut();
     const authenticationParams = {
       email: 'any_email@email.com',
       password: 'any_password',
     };
 
-    sut.perform(authenticationParams);
+    await sut.perform(authenticationParams);
 
     expect(loadUserRepositorySpy.params).toEqual({ email: 'any_email@email.com' });
     expect(loadUserRepositorySpy.callsCount).toBe(1);
