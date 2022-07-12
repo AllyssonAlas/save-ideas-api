35526b07c13a32a433c2cb607f067ff00cb284de
AllyssonAlas <allyssonmgs@gmail.com>
2022-07-12 16:00:12 -0300

    feat: ensure AuthenticationController returns an error if  Validation returns an error

--- src/presentation/controllers/authentication-controller.ts
+++ src/presentation/controllers/authentication-controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Validation } from '@/presentation/protocols';
+import { badRequest } from '@/presentation/helpers';
 
 export class AuthenticationController implements Controller {
   constructor(private readonly validation: Validation) {}
 
   async handle(request: AuthenticationController.Request): Promise<any> {
-    this.validation.validate(request);
+    const error = this.validation.validate(request);
+    if (error) {
+      return badRequest(error);
+    }
     return Promise.resolve();
   }
 }
