d968dd9664e1f91e60d4e0138ff53d30f3ab7b93
AllyssonAlas <allyssonmgs@gmail.com>
2022-07-12 11:38:16 -0300

    refactor: move sut creation to a factory helper method

--- tests/infra/gateways/jwt-adapter.spec.ts
+++ tests/infra/gateways/jwt-adapter.spec.ts
@@ -2,13 +2,24 @@ import jwt from 'jsonwebtoken';
 
 import { JwtAdapter } from '@/infra/gateways';
 
+interface SutTypes {
+  sut: JwtAdapter;
+  secret: string;
+}
+
+const makeSut = (): SutTypes => {
+  const secret = 'secret';
+  const sut = new JwtAdapter('secret');
+  return { sut, secret };
+};
+
 describe('JwtAdapter', () => {
   test('Should call sign with correct values', async () => {
-    const sut = new JwtAdapter('secret');
+    const { sut, secret } = makeSut();
     const signSpy = jest.spyOn(jwt, 'sign');
 
     await sut.encrypt({ plaintext: 'any_id' });
 
-    expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret');
+    expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, secret);
   });
 });
