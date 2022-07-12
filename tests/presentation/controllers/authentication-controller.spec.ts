b193992915c6c47bd5914709f9252a86bda14dd8
AllyssonAlas <allyssonmgs@gmail.com>
2022-07-12 15:54:39 -0300

    refactor: move sut creation to a factory method

--- tests/presentation/controllers/authentication-controller.spec.ts
+++ tests/presentation/controllers/authentication-controller.spec.ts
@@ -2,10 +2,20 @@ import { AuthenticationController } from '@/presentation/controllers';
 
 import { ValidationSpy } from '@/tests/presentation/mocks';
 
+interface SutTypes {
+  sut: AuthenticationController;
+  validationSpy: ValidationSpy;
+}
+
+const makeSut = (): SutTypes => {
+  const validationSpy = new ValidationSpy();
+  const sut = new AuthenticationController(validationSpy);
+  return { sut, validationSpy };
+};
+
 describe('AuthenticationController', () => {
   test('Should call Validation with correct values', async () => {
-    const validationSpy = new ValidationSpy();
-    const sut = new AuthenticationController(validationSpy);
+    const { sut, validationSpy } = makeSut();
     const request = {
       email: 'any_email@mail.com',
       password: 'any_password',
