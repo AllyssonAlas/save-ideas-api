3485a2388974767fc0b912d07ee2d3554f914ff6
AllyssonAlas <allyssonmgs@gmail.com>
2022-07-12 13:02:20 -0300

    feat: ensure JwtAdapter return a token on success

--- src/infra/gateways/jwt-adapter.ts
+++ src/infra/gateways/jwt-adapter.ts
@@ -5,7 +5,8 @@ import { Encrypter } from '@/data/protocols/gateways';
 export class JwtAdapter {
   constructor(private readonly secret: string) {}
 
-  async encrypt(params: Encrypter.Params): Promise<void> {
-    await jwt.sign({ id: params.plaintext }, this.secret);
+  async encrypt(params: Encrypter.Params): Promise<Encrypter.Result> {
+    const ciphertext = await jwt.sign({ id: params.plaintext }, this.secret);
+    return { ciphertext };
   }
 }
