a02f98ef0fc196c0d192e3b6aadb22e411db08df
AllyssonAlas <allyssonmgs@gmail.com>
2022-07-13 14:48:58 -0300

    feat: ensure loadById returns user data on success

--- src/infra/db/firestore/user-repository.ts
+++ src/infra/db/firestore/user-repository.ts
@@ -2,11 +2,17 @@ import {
   CreateUserRepository,
   LoadUserByEmailRepository,
   UpdateUserRepository,
+  LoadUserByIdRepository,
 } from '@/data/protocols/repositories';
 import { FirestoreHelper } from '@/infra/db';
 
 // eslint-disable-next-line prettier/prettier
-export class UserRepository implements CreateUserRepository, LoadUserByEmailRepository, UpdateUserRepository {
+export class UserRepository implements
+    CreateUserRepository,
+    LoadUserByEmailRepository,
+    UpdateUserRepository,
+    LoadUserByIdRepository
+{
   async create(params: CreateUserRepository.Params): Promise<CreateUserRepository.Result> {
     const usersCollection = FirestoreHelper.getCollection('users');
     const result = await usersCollection.add(params);
@@ -28,5 +34,11 @@ export class UserRepository implements CreateUserRepository, LoadUserByEmailRepo
     const usersCollection = FirestoreHelper.getCollection('users');
     const docRef = usersCollection.doc(id);
     await docRef.update(userData);
+  }
+
+  async loadById(params: LoadUserByIdRepository.Params): Promise<LoadUserByIdRepository.Result> {
+    const usersCollection = FirestoreHelper.getCollection('users');
+    const docData = await usersCollection.doc(params.id).get();
+    return FirestoreHelper.documentMapper(docData);
   }
 }
