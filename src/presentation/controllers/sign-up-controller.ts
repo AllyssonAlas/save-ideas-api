e82da662002803220e3c4363f4f9f34aecff5070
Allysson <allyssonmgs@gmail.com>
2022-07-12 21:11:30 -0300

    feat: ensure SignUpController calls Authentication with correct values

--- src/presentation/controllers/sign-up-controller.ts
+++ src/presentation/controllers/sign-up-controller.ts
@@ -1,10 +1,14 @@
-import { SignUp } from '@/domain/usecases';
+import { Authentication, SignUp } from '@/domain/usecases';
 import { HttpResponse, Controller, Validation } from '@/presentation/protocols';
 import { EmailInUseError } from '@/presentation/errors';
 import { badRequest, serverError, noContent, forbidden } from '@/presentation/helpers';
 
 export class SignUpController implements Controller {
-  constructor(private readonly validation: Validation, private readonly signUp: SignUp) {}
+  constructor(
+    private readonly validation: Validation,
+    private readonly signUp: SignUp,
+    private readonly authentication: Authentication,
+  ) {}
 
   async handle(request: SignUpController.Request): Promise<HttpResponse> {
     try {
@@ -13,6 +17,7 @@ export class SignUpController implements Controller {
       const { passwordConfirmation, ...userData } = request;
       const { wasSigned } = await this.signUp.perform(userData);
       if (!wasSigned) return forbidden(new EmailInUseError());
+      await this.authentication.perform({ email: userData.email, password: userData.password });
       return noContent();
     } catch (error) {
       return serverError(error);
