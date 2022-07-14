9e93eb21ba3803bc1520936421809ea718febacc
Allysson <allyssonmgs@gmail.com>
2022-07-14 16:26:45 -0300

    feat: ensure UpdateUserController return an error if Validation returns an error

--- src/presentation/controllers/update-user-controller.ts
+++ src/presentation/controllers/update-user-controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Validation } from '@/presentation/protocols';
-
+import { badRequest } from '@/presentation/helpers';
 export class UpdateUserController implements Controller {
   constructor(private readonly validation: Validation) {}
 
   async handle(request: UpdateUserController.Request): Promise<any> {
-    this.validation.validate(request);
+    const error = this.validation.validate(request);
+    if (error) {
+      return badRequest(error);
+    }
     return Promise.resolve();
   }
 }
