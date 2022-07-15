25e23e4d356597c5b17574f2a9e966f5be70d0a7
Allysson <allyssonmgs@gmail.com>
2022-07-14 21:10:50 -0300

    feat: ensure makeUpdateUserValidation calls ValidationComposite with correct validations

---
+++ src/main/factories/validations/update-user-validation-factory.ts
@@ -0,0 +1,16 @@
+import { Validation } from '@/presentation/protocols';
+import { EmailValidatorAdapter } from '@/presentation/utils';
+import {
+  EmailValidation,
+  RequiredFieldValidation,
+  ValidationComposite,
+} from '@/presentation/validators';
+
+export const makeUpdateUserValidation = (): Validation => {
+  const validations: Validation[] = [];
+  for (const field of ['id', 'name', 'email']) {
+    validations.push(new RequiredFieldValidation(field));
+  }
+  validations.push(new EmailValidation('email', new EmailValidatorAdapter()));
+  return new ValidationComposite(validations);
+};
