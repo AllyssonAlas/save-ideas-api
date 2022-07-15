8a133d4785129c8c66ce31fc28c6ce1b87e1e783
AllyssonAlas <allyssonmgs@gmail.com>
2022-07-15 08:46:44 -0300

    test: add CompareFieldsValidation to newPassword and newPasswordConfirmation fields

--- tests/main/factories/validations/update-user-validation-factory.spec.ts
+++ tests/main/factories/validations/update-user-validation-factory.spec.ts
@@ -1,6 +1,7 @@
 import { Validation } from '@/presentation/protocols';
 import { EmailValidatorAdapter } from '@/presentation/utils';
 import {
+  CompareFieldsValidation,
   EmailValidation,
   RequiredFieldValidation,
   ValidationComposite,
@@ -17,6 +18,7 @@ describe('UpdateUserValidationFactory', () => {
     for (const field of ['id', 'name', 'email']) {
       validations.push(new RequiredFieldValidation(field));
     }
+    validations.push(new CompareFieldsValidation('newPassword', 'newPasswordConfirmation'));
     validations.push(new EmailValidation('email', new EmailValidatorAdapter()));
 
     expect(ValidationComposite).toHaveBeenCalledWith(validations);
