9dd2e965ed85a2843dae260e8fc2249bf9ae18d6
AllyssonAlas <allyssonmgs@gmail.com>
2022-07-15 15:55:18 -0300

    refactor: add LoadUserByEmailRepository to UpdateUserUsecase instance

--- src/main/factories/usecases/update-user-usecase-factory.ts
+++ src/main/factories/usecases/update-user-usecase-factory.ts
@@ -4,5 +4,10 @@ import { makeBcryptAdapter } from '@/main/factories/gateways';
 
 export const makeUpdateUserUseCase = (): UpdateUserUsecase => {
   const userRepository = new UserRepository();
-  return new UpdateUserUsecase(makeBcryptAdapter(), makeBcryptAdapter(), userRepository);
+  return new UpdateUserUsecase(
+    makeBcryptAdapter(),
+    makeBcryptAdapter(),
+    userRepository,
+    userRepository,
+  );
 };
