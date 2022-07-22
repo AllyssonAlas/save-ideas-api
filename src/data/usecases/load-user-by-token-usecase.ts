f64ec4f03d2a5024922a0a5435f32e4f1cdbac3e
Allysson <allyssonmgs@gmail.com>
2022-07-22 18:49:33 -0300

    feat: ensure LoadUserByTokenUsecase calls LoadUserByFieldRepository with correct value

--- src/data/usecases/load-user-by-token-usecase.ts
+++ src/data/usecases/load-user-by-token-usecase.ts
@@ -1,11 +1,18 @@
 import { LoadUserByToken } from '@/domain/usecases';
 import { Decrypter } from '@/data/protocols/gateways';
+import { LoadUserByFieldRepository } from '@/data/protocols/repositories';
 
 export class LoadUserByTokenUsecase implements LoadUserByToken {
-  constructor(private readonly decrypter: Decrypter) {}
+  constructor(
+    private readonly decrypter: Decrypter,
+    private readonly loadUserByFieldRepository: LoadUserByFieldRepository,
+  ) {}
 
   async perform(params: LoadUserByToken.Params): Promise<LoadUserByToken.Result> {
-    await this.decrypter.decrypt({ ciphertext: params.accessToken });
+    const { isTokenValid } = await this.decrypter.decrypt({ ciphertext: params.accessToken });
+    if (isTokenValid) {
+      await this.loadUserByFieldRepository.loadByField({ accessToken: params.accessToken });
+    }
     return Promise.resolve(null);
   }
 }
