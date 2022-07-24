d201480b2f0b84852a7008e2789684f3e06ff14f
Allysson <allyssonmgs@gmail.com>
2022-07-24 15:40:07 -0300

    feat: ensure AuthMiddleware returns 500 if LoadUserByToken throws

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,18 +1,22 @@
 import { LoadUserByToken } from '@/domain/usecases';
 import { HttpResponse, Middleware } from '@/presentation/protocols';
-import { forbidden, ok } from '@/presentation/helpers';
+import { forbidden, ok, serverError } from '@/presentation/helpers';
 import { AccessDeniedError } from '@/presentation/errors';
 
 export class AuthMiddleware implements Middleware {
   constructor(private readonly loadUserByToken: LoadUserByToken) {}
 
   async handle(request: any): Promise<HttpResponse> {
-    if (request.accessToken) {
-      const user = await this.loadUserByToken.perform({ accessToken: request.accessToken });
-      if (user) {
-        return ok({ userId: user.id });
+    try {
+      if (request.accessToken) {
+        const user = await this.loadUserByToken.perform({ accessToken: request.accessToken });
+        if (user) {
+          return ok({ userId: user.id });
+        }
       }
+      return forbidden(new AccessDeniedError());
+    } catch (error) {
+      return serverError(error);
     }
-    return forbidden(new AccessDeniedError());
   }
 }
