a76eab7d993e86e133f354d907516ec6d9ab31c4
Allysson <allyssonmgs@gmail.com>
2022-07-30 20:38:51 -0300

    feat: ensure CreateIdeiaController calls LoadUserById with correct value

--- src/presentation/controllers/ideia/create-ideia-controller.ts
+++ src/presentation/controllers/ideia/create-ideia-controller.ts
@@ -1,9 +1,12 @@
-import { CreateIdeia } from '@/domain/usecases';
+import { CreateIdeia, LoadUserById } from '@/domain/usecases';
 import { Controller, Validation } from '@/presentation/protocols';
 import { badRequest, serverError } from '@/presentation/helpers';
 
 export class CreateIdeiaController implements Controller {
-  constructor(private readonly validation: Validation) {}
+  constructor(
+    private readonly validation: Validation,
+    private readonly loadUserById: LoadUserById,
+  ) {}
 
   async handle(request: CreateIdeiaController.Request): Promise<any> {
     try {
@@ -11,6 +14,7 @@ export class CreateIdeiaController implements Controller {
       if (error) {
         return badRequest(error);
       }
+      await this.loadUserById.perform({ id: request.userId });
       return Promise.resolve(null);
     } catch (error) {
       return serverError(error);
