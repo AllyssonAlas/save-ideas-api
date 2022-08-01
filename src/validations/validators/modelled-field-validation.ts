3e55182c3d065c22d95c593b4552a7db0a8ea854
Allysson <allyssonmgs@gmail.com>
2022-08-01 20:19:17 -0300

    feat: ensure ModelledFieldValidation returns an InvalidParamError if subfields are incorrect

--- src/validations/validators/modelled-field-validation.ts
+++ src/validations/validators/modelled-field-validation.ts
@@ -5,8 +5,15 @@ export class ModelledFieldValidation implements Validation {
   constructor(private readonly fieldName: string, private readonly subFields: string[]) {}
 
   validate(input: any): Error | undefined {
-    if (!Array.isArray(input[this.fieldName])) {
+    const fieldValue = input[this.fieldName];
+    if (!Array.isArray(fieldValue)) {
       return new InvalidParamError(this.fieldName);
+    }
+    for (const field of fieldValue) {
+      const subFieldsKeys = Object.keys(field);
+      if (!subFieldsKeys.some((subFieldKey) => this.subFields.includes(subFieldKey))) {
+        return new InvalidParamError(this.fieldName);
+      }
     }
   }
 }
