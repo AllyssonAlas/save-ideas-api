105ff9e065f8273381dc8251bc60ea0e9ca76f21
Allysson <allyssonmgs@gmail.com>
2022-08-03 21:33:50 -0300

    feat: ensure UpdateUserUsecase calls LoadUserByFieldRepository with correct values

--- src/data/usecases/user/update-user-usecase.ts
+++ src/data/usecases/user/update-user-usecase.ts
@@ -1,10 +1,17 @@
 import { UpdateUser } from '@/domain/usecases';
-import { LoadUserByIdRepository } from '@/data/protocols/repositories';
+import { LoadUserByIdRepository, LoadUserByFieldRepository } from '@/data/protocols/repositories';
 
 export class UpdateUserUsecase implements UpdateUser {
-  constructor(private readonly loadUserByIdRepository: LoadUserByIdRepository) {}
+  constructor(
+    private readonly loadUserByIdRepository: LoadUserByIdRepository,
+    private readonly loadUserByFieldRepository: LoadUserByFieldRepository,
+  ) {}
 
   async perform(params: UpdateUser.Params): Promise<any> {
-    await this.loadUserByIdRepository.loadById({ id: params.userId });
+    const { userId, email } = params;
+    const userData = await this.loadUserByIdRepository.loadById({ id: userId });
+    if (userData?.email !== email) {
+      await this.loadUserByFieldRepository.loadByField({ email });
+    }
   }
 }
