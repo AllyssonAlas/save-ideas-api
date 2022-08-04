800a2691c75a4725d6197fc7f1fb0cd83cdf01c9
Allysson <allyssonmgs@gmail.com>
2022-08-04 20:46:28 -0300

    feat: ensure UpdateUserUsecase calls HasherComparer with correct value

--- src/data/usecases/user/update-user-usecase.ts
+++ src/data/usecases/user/update-user-usecase.ts
@@ -1,20 +1,28 @@
 import { UpdateUser } from '@/domain/usecases';
 import { LoadUserByIdRepository, LoadUserByFieldRepository } from '@/data/protocols/repositories';
+import { HasherComparer } from '@/data/protocols/gateways';
 
 export class UpdateUserUsecase implements UpdateUser {
   constructor(
     private readonly loadUserByIdRepository: LoadUserByIdRepository,
     private readonly loadUserByFieldRepository: LoadUserByFieldRepository,
+    private readonly hasherComparer: HasherComparer,
   ) {}
 
   async perform(params: UpdateUser.Params): Promise<any> {
-    const { userId, email } = params;
+    const { userId, email, password } = params;
     const authedUserData = await this.loadUserByIdRepository.loadById({ id: userId });
     if (authedUserData?.email !== email) {
       const userData = await this.loadUserByFieldRepository.loadByField({ email });
       if (userData) {
         return { success: false, invalidField: 'email' };
       }
+    }
+    if (password) {
+      await this.hasherComparer.compare({
+        plaintext: password,
+        digest: authedUserData?.password || '',
+      });
     }
   }
 }
