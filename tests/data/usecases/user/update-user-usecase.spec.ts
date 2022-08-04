a7a036ada4cf1237fa10ed2bca524d15790b1aa1
Allysson <allyssonmgs@gmail.com>
2022-08-03 21:16:00 -0300

    refactor: move sut creation to a factory method

--- tests/data/usecases/user/update-user-usecase.spec.ts
+++ tests/data/usecases/user/update-user-usecase.spec.ts
@@ -3,10 +3,20 @@ import { UpdateUserUsecase } from '@/data/usecases';
 import { mockUpdateUserParams } from '@/tests/domain/mocks';
 import { LoadUserByIdRepositorySpy } from '@/tests/data/mocks';
 
+interface SutTypes {
+  sut: UpdateUserUsecase;
+  loadUserByIdRepositorySpy: LoadUserByIdRepositorySpy;
+}
+
+const makeSut = (): SutTypes => {
+  const loadUserByIdRepositorySpy = new LoadUserByIdRepositorySpy();
+  const sut = new UpdateUserUsecase(loadUserByIdRepositorySpy);
+  return { sut, loadUserByIdRepositorySpy };
+};
+
 describe('UpdateUserUsecase', () => {
   test('Should call LoadUserByIdRepository with correct value', async () => {
-    const loadUserByIdRepositorySpy = new LoadUserByIdRepositorySpy();
-    const sut = new UpdateUserUsecase(loadUserByIdRepositorySpy);
+    const { sut, loadUserByIdRepositorySpy } = makeSut();
 
     await sut.perform(mockUpdateUserParams());
 
