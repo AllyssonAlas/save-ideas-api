af83983295ce92138de82827645826de885c6207
Allysson <allyssonmgs@gmail.com>
2022-08-07 18:50:02 -0300

    feat: ensure RelatedRequiredFieldsValidation returns MissingParamError if validation fails

--- src/validations/validators/index.ts
+++ src/validations/validators/index.ts
@@ -1,5 +1,6 @@
 export * from './compare-fields-validation';
 export * from './email-validation';
 export * from './modelled-field-validation';
+export * from './related-required-fields-validation';
 export * from './required-field-validation';
 export * from './validation-composite';
