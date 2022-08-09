1740c2feb62a1d5c6fc4bdccd12b56b198b2d96a
Allysson <allyssonmgs@gmail.com>
2022-08-09 19:50:07 -0300

    feat: ensure ListIdeiasUsecase returns a list of ideias on success

--- src/data/usecases/ideia/list-ideias-usecase.ts
+++ src/data/usecases/ideia/list-ideias-usecase.ts
@@ -4,7 +4,7 @@ import { ListIdeiasRepository } from '@/data/protocols/repositories';
 export class ListIdeiasUsecase implements ListIdeias {
   constructor(private readonly listIdeiasRepository: ListIdeiasRepository) {}
 
-  async perform(params: ListIdeias.Params): Promise<any> {
-    await this.listIdeiasRepository.list(params);
+  async perform(params: ListIdeias.Params): Promise<ListIdeias.Result> {
+    return this.listIdeiasRepository.list(params);
   }
 }
