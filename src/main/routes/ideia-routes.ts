d8ae700c0d0243083075c2a2ccfc8f83d565a3cd
Allysson <allyssonmgs@gmail.com>
2022-08-10 20:20:04 -0300

    feat: ensure /ideias returns 204 on empty ideias list

--- src/main/routes/ideia-routes.ts
+++ src/main/routes/ideia-routes.ts
@@ -2,8 +2,9 @@ import { Router } from 'express';
 
 import { adaptRoute } from '@/main/adapters';
 import { auth } from '@/main/middlewares';
-import { makeCreateIdeiaController } from '@/main/factories/controllers';
+import { makeCreateIdeiaController, makeListIdeiasController } from '@/main/factories/controllers';
 
 export default (router: Router): void => {
   router.post('/ideia', auth, adaptRoute(makeCreateIdeiaController()));
+  router.get('/ideias', auth, adaptRoute(makeListIdeiasController()));
 };
