880dd1ba05fa6e364e7dd979f67f430397632fe1
Allysson <allyssonmgs@gmail.com>
2022-08-10 19:54:24 -0300

    feat: ensure ListIdeiasController returns 500 if ListIdeias throws

--- src/presentation/controllers/ideia/list-ideias-controller.ts
+++ src/presentation/controllers/ideia/list-ideias-controller.ts
@@ -1,11 +1,16 @@
 import { ListIdeias } from '@/domain/usecases';
 import { Controller } from '@/presentation/protocols';
+import { serverError } from '@/presentation/helpers';
 
 export class ListIdeiasController implements Controller {
   constructor(private readonly listIdeias: ListIdeias) {}
 
   async handle(request: ListIdeiasController.Request): Promise<any> {
-    await this.listIdeias.perform(request);
+    try {
+      await this.listIdeias.perform(request);
+    } catch (error) {
+      return serverError(error);
+    }
   }
 }
 
