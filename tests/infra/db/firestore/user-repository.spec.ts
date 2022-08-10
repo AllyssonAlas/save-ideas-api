20f02ef384a76167b193343887ad3671da75088f
Allysson <allyssonmgs@gmail.com>
2022-08-09 22:12:04 -0300

    refactor: simplify tests

--- tests/infra/db/firestore/user-repository.spec.ts
+++ tests/infra/db/firestore/user-repository.spec.ts
@@ -1,7 +1,7 @@
 import { FirestoreHelper, UserRepository } from '@/infra/db';
 import { CollectionReference } from 'firebase-admin/firestore';
 
-import { mockSignUpParams } from '@/tests/domain/mocks';
+import { mockSignUpParams, mockUpdateUserWithAdditionalValuesParams } from '@/tests/domain/mocks';
 
 const makeSut = (): UserRepository => {
   return new UserRepository();
@@ -31,11 +31,7 @@ describe('UserRepository', () => {
     test('Should return true on success', async () => {
       const sut = makeSut();
 
-      const user = await sut.create({
-        name: 'any_name',
-        email: 'any_email@example.com',
-        password: 'any_password',
-      });
+      const user = await sut.create(mockSignUpParams());
 
       expect(user).toBeTruthy();
     });
@@ -67,22 +63,13 @@ describe('UserRepository', () => {
   describe('update()', () => {
     test('Should return an user update with the data received', async () => {
       const sut = makeSut();
-      const createUserParams = mockSignUpParams();
-      await usersCollection.add(createUserParams);
+      await usersCollection.doc('any_id').set(mockSignUpParams());
 
-      const user = await sut.loadByField({ email: createUserParams.email });
-      await sut.update({
-        id: user?.id || 'any_id',
-        name: 'other_name',
-        email: 'other_mail@mail.com',
-        password: 'other_password',
-        accessToken: 'any_token',
-      });
-      const updatedUser = await sut.loadByField({ email: 'other_mail@mail.com' });
+      await sut.update(mockUpdateUserWithAdditionalValuesParams());
 
-      expect(updatedUser?.name).toBe('other_name');
-      expect(updatedUser?.password).toBe('other_password');
-      expect(updatedUser?.accessToken).toBe('any_token');
+      const updatedUser = await sut.loadById({ id: 'any_id' });
+      expect(updatedUser.name).toBe('other_name');
+      expect(updatedUser.email).toBe('other_email@mail.com');
     });
   });
 
@@ -90,19 +77,12 @@ describe('UserRepository', () => {
     test('Should return an user if id is valid', async () => {
       const sut = makeSut();
 
-      await usersCollection.doc('any_id').set({
-        name: 'any_name',
-        email: 'any_email@email.com',
-        password: 'any_password',
-        accessToken: 'any_access_token',
-      });
+      await usersCollection.doc('any_id').set(mockSignUpParams());
       const user = await sut.loadById({ id: 'any_id' });
 
       expect(user).toBeTruthy();
-      expect(user?.name).toBe('any_name');
-      expect(user?.email).toBe('any_email@email.com');
-      expect(user?.password).toBe('any_password');
-      expect(user?.accessToken).toBe('any_access_token');
+      expect(user.name).toBe('any_name');
+      expect(user.email).toBe('any_email@mail.com');
     });
   });
 });
