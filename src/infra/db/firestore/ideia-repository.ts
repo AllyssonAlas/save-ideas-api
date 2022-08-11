90e96fa3387b51a7824bf4588bdbe4148c29db4b
Allysson <allyssonmgs@gmail.com>
2022-08-11 20:05:22 -0300

    feat: ensure IdeiaRepository load returns an ideia on success

--- src/infra/db/firestore/ideia-repository.ts
+++ src/infra/db/firestore/ideia-repository.ts
@@ -1,7 +1,12 @@
-import { CreateIdeiaRepository, ListIdeiasRepository } from '@/data/protocols/repositories';
+import {
+  CreateIdeiaRepository,
+  ListIdeiasRepository,
+  LoadIdeiaByIdRepository,
+} from '@/data/protocols/repositories';
 import { FirestoreHelper } from '@/infra/db';
 
-export class IdeiaRepository implements CreateIdeiaRepository, ListIdeiasRepository {
+// eslint-disable-next-line prettier/prettier
+export class IdeiaRepository implements CreateIdeiaRepository, ListIdeiasRepository, LoadIdeiaByIdRepository {
   async create(params: CreateIdeiaRepository.Params): Promise<CreateIdeiaRepository.Result> {
     const ideiasCollection = FirestoreHelper.getCollection('ideias');
     const ideiaRef = await ideiasCollection.add(params);
@@ -14,5 +19,11 @@ export class IdeiaRepository implements CreateIdeiaRepository, ListIdeiasReposit
     const ideiasSnapshot = await ideiasCollection.where('ownerId', '==', params.userId).get();
     const ideias = FirestoreHelper.collectionMapper(ideiasSnapshot);
     return ideias;
+  }
+
+  async load(params: LoadIdeiaByIdRepository.Params): Promise<LoadIdeiaByIdRepository.Result> {
+    const ideiasCollection = FirestoreHelper.getCollection('ideias');
+    const docData = await ideiasCollection.doc(params.ideiaId).get();
+    return FirestoreHelper.documentMapper(docData);
   }
 }
