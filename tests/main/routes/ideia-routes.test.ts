882b1ee91f1d1a5925af92b7902abd930204c2a0
Allysson <allyssonmgs@gmail.com>
2022-08-10 21:18:49 -0300

    test: ensure /ideias returns 200 on success

--- tests/main/routes/ideia-routes.test.ts
+++ tests/main/routes/ideia-routes.test.ts
@@ -5,19 +5,21 @@ import { FirestoreHelper } from '@/infra/db';
 
 import app from '@/main/config/app';
 
+import { mockCreateIdeiaParams } from '@/tests/domain/mocks';
 import { mockAcessToken } from '@/tests/main/mocks';
 
 describe('Ideia Routes', () => {
+  let usersCollection: CollectionReference;
   let ideiasCollection: CollectionReference;
 
   beforeAll(() => {
     FirestoreHelper.connect();
     ideiasCollection = FirestoreHelper.getCollection('ideias');
+    usersCollection = FirestoreHelper.getCollection('users');
   });
 
   afterEach(async () => {
-    const userCollection = FirestoreHelper.getCollection('users');
-    const users = await userCollection.listDocuments();
+    const users = await usersCollection.listDocuments();
 
     for (let index = 0; index < users.length; index++) {
       await users[index].delete();
@@ -80,6 +82,17 @@ describe('Ideia Routes', () => {
       const accessToken = await mockAcessToken();
 
       await request(app).get('/api/ideias').set('x-access-token', accessToken).expect(204);
+    });
+
+    test('Should return 200 on success', async () => {
+      const accessToken = await mockAcessToken();
+
+      const usersSnapshot = await usersCollection.get();
+      const users = FirestoreHelper.collectionMapper(usersSnapshot);
+      const ideiaData = { ownerId: users[0].id, ...mockCreateIdeiaParams() };
+      await ideiasCollection.add(ideiaData);
+
+      await request(app).get('/api/ideias').set('x-access-token', accessToken).expect(200);
     });
   });
 });
