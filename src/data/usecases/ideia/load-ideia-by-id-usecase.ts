38867e0124615134b0eff686fff39d4e84088ed5
Allysson <allyssonmgs@gmail.com>
2022-08-11 19:49:55 -0300

    feat: ensure LoadIdeiaByIdUsecase returns an ideia on success

--- src/data/usecases/ideia/load-ideia-by-id-usecase.ts
+++ src/data/usecases/ideia/load-ideia-by-id-usecase.ts
@@ -4,7 +4,7 @@ import { LoadIdeiaByIdRepository } from '@/data/protocols/repositories';
 export class LoadIdeiaByIdUsecase implements LoadIdeiaById {
   constructor(private readonly loadIdeiaByIdRepository: LoadIdeiaByIdRepository) {}
 
-  async perform(params: LoadIdeiaById.Params): Promise<any> {
+  async perform(params: LoadIdeiaById.Params): Promise<LoadIdeiaById.Result> {
     return this.loadIdeiaByIdRepository.load(params);
   }
 }
