117b7d6fd73e4df02e2e3d7270fad1961535cb5b
Allysson <allyssonmgs@gmail.com>
2022-08-12 21:29:10 -0300

    feat: ensure ValidateIdeiaIdMiddleware returns 500 if LoadIdeiaById throws

--- src/presentation/middlewares/validate-ideia-id-middleware.ts
+++ src/presentation/middlewares/validate-ideia-id-middleware.ts
@@ -1,16 +1,20 @@
 import { LoadIdeiaById } from '@/domain/usecases';
 import { HttpResponse, Middleware } from '@/presentation/protocols';
-import { forbidden } from '@/presentation/helpers';
+import { forbidden, serverError } from '@/presentation/helpers';
 import { MissingParamError } from '@/presentation/errors';
 
 export class ValidateIdeiaIdMiddleware implements Middleware {
   constructor(private readonly loadIdeiaById: LoadIdeiaById) {}
 
   async handle(request: ValidateIdeiaIdMiddleware.Request): Promise<HttpResponse> {
-    if (request.ideiaId) {
-      await this.loadIdeiaById.perform({ ideiaId: request.ideiaId });
+    try {
+      if (request.ideiaId) {
+        await this.loadIdeiaById.perform({ ideiaId: request.ideiaId });
+      }
+      return Promise.resolve(forbidden(new MissingParamError('ideiaId')));
+    } catch (error) {
+      return serverError(error);
     }
-    return Promise.resolve(forbidden(new MissingParamError('ideiaId')));
   }
 }
 
