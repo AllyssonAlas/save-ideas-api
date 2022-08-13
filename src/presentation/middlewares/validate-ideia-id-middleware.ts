e4e64504b66f9b2cbd32759be2ef55ba0059ee76
Allysson <allyssonmgs@gmail.com>
2022-08-12 21:25:47 -0300

    feat: ensure ValidateIdeiaIdMiddleware calls LoadIdeiaById with correct ideiaId

--- src/presentation/middlewares/validate-ideia-id-middleware.ts
+++ src/presentation/middlewares/validate-ideia-id-middleware.ts
@@ -1,11 +1,15 @@
+import { LoadIdeiaById } from '@/domain/usecases';
 import { HttpResponse, Middleware } from '@/presentation/protocols';
 import { forbidden } from '@/presentation/helpers';
 import { MissingParamError } from '@/presentation/errors';
 
 export class ValidateIdeiaIdMiddleware implements Middleware {
-  constructor() {}
+  constructor(private readonly loadIdeiaById: LoadIdeiaById) {}
 
   async handle(request: ValidateIdeiaIdMiddleware.Request): Promise<HttpResponse> {
+    if (request.ideiaId) {
+      await this.loadIdeiaById.perform({ ideiaId: request.ideiaId });
+    }
     return Promise.resolve(forbidden(new MissingParamError('ideiaId')));
   }
 }
