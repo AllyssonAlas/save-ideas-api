46819fd837a6b7747c49f47c3c83bfe9910e5a30
Allysson <allyssonmgs@gmail.com>
2022-08-12 21:26:08 -0300

    test: ensure ValidateIdeiaIdMiddleware calls LoadIdeiaById with correct ideiaId

--- tests/presentation/middlewares/validate-ideia-id-middleware.spec.ts
+++ tests/presentation/middlewares/validate-ideia-id-middleware.spec.ts
@@ -2,13 +2,19 @@ import { ValidateIdeiaIdMiddleware } from '@/presentation/middlewares';
 import { MissingParamError } from '@/presentation/errors';
 import { forbidden } from '@/presentation/helpers';
 
+import { LoadIdeiaByIdUsecaseSpy } from '@/tests/presentation/mocks';
+
+const mockRequest = (): ValidateIdeiaIdMiddleware.Request => ({ ideiaId: 'any_ideia_id' });
+
 interface SutTypes {
   sut: ValidateIdeiaIdMiddleware;
+  loadIdeiaByIdUsecaseSpy: LoadIdeiaByIdUsecaseSpy;
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new ValidateIdeiaIdMiddleware();
-  return { sut };
+  const loadIdeiaByIdUsecaseSpy = new LoadIdeiaByIdUsecaseSpy();
+  const sut = new ValidateIdeiaIdMiddleware(loadIdeiaByIdUsecaseSpy);
+  return { sut, loadIdeiaByIdUsecaseSpy };
 };
 
 describe('AuthMiddleware', () => {
@@ -18,5 +24,13 @@ describe('AuthMiddleware', () => {
     const httpResponse = await sut.handle({});
 
     expect(httpResponse).toEqual(forbidden(new MissingParamError('ideiaId')));
+  });
+
+  test('Should call LoadIdeiaByIdUsescase with correct ideiaId', async () => {
+    const { sut, loadIdeiaByIdUsecaseSpy } = makeSut();
+
+    await sut.handle(mockRequest());
+
+    expect(loadIdeiaByIdUsecaseSpy.params).toEqual({ ideiaId: 'any_ideia_id' });
   });
 });
--- tests/presentation/mocks/mock-ideia.ts
+++ tests/presentation/mocks/mock-ideia.ts
@@ -1,4 +1,4 @@
-import { CreateIdeia, ListIdeias } from '@/domain/usecases';
+import { CreateIdeia, ListIdeias, LoadIdeiaById } from '@/domain/usecases';
 
 export class CreateIdeiaUsecaseSpy implements CreateIdeia {
   params?: CreateIdeia.Params;
@@ -59,6 +59,31 @@ export class ListIdeiasUsecaseSpy implements ListIdeias {
   callsCount = 0;
 
   async perform(params: ListIdeias.Params): Promise<ListIdeias.Result> {
+    this.params = params;
+    this.callsCount++;
+    return Promise.resolve(this.result);
+  }
+}
+
+export class LoadIdeiaByIdUsecaseSpy implements LoadIdeiaById {
+  params?: LoadIdeiaById.Params;
+  result = {
+    id: 'any_ideia_id',
+    ownerId: 'any_user_id',
+    title: 'any_title_ideia',
+    description: 'any_description_ideia',
+    features: [
+      {
+        name: 'any_feature_name',
+        description: 'any_feature_description',
+        finished: false,
+      },
+    ],
+  };
+
+  callsCount = 0;
+
+  async perform(params: LoadIdeiaById.Params): Promise<LoadIdeiaById.Result> {
     this.params = params;
     this.callsCount++;
     return Promise.resolve(this.result);
