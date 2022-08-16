d9f0dde0f8c771ab79990d942763cf1151d104eb
Allysson <allyssonmgs@gmail.com>
2022-08-16 19:22:12 -0300

    feat: add DELETE /idea/:ideaId route

--- src/main/routes/idea-routes.ts
+++ src/main/routes/idea-routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 
 import { adaptRoute } from '@/main/adapters';
-import { auth } from '@/main/middlewares';
-import { makeCreateIdeaController, makeListIdeasController } from '@/main/factories/controllers';
+import { auth, validateIdeaId } from '@/main/middlewares';
+import {
+  makeCreateIdeaController,
+  makeListIdeasController,
+  makeDeleteIdeaController,
+} from '@/main/factories/controllers';
 
 export default (router: Router): void => {
   router.post('/idea', auth, adaptRoute(makeCreateIdeaController()));
   router.get('/ideas', auth, adaptRoute(makeListIdeasController()));
+  router.delete('/idea/:ideaId', auth, validateIdeaId, adaptRoute(makeDeleteIdeaController()));
 };
