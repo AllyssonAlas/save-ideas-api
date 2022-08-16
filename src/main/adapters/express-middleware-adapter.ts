107cac91c5e02ff4c758b6dbf93442c8439c83b4
Allysson <allyssonmgs@gmail.com>
2022-08-16 19:21:29 -0300

    fix: amend adaptMiddleware request variable

--- src/main/adapters/express-middleware-adapter.ts
+++ src/main/adapters/express-middleware-adapter.ts
@@ -6,6 +6,8 @@ export const adaptMiddleware = (middleware: Middleware) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     const request = {
       accessToken: req.headers?.['x-access-token'],
+      userId: req.userId || '',
+      ideaId: req.params.ideaId || '',
       ...(req.headers || {}),
     };
     const httpResponse = await middleware.handle(request);
