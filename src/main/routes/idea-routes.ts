936058aab12ea6b54cd6d7ad19ca2c28499066e5
Allysson <allyssonmgs@gmail.com>
2022-08-17 20:31:42 -0300

    feat: ensure PUT /idea returns 204 on success

--- src/main/routes/idea-routes.ts
+++ src/main/routes/idea-routes.ts
@@ -6,10 +6,12 @@ import {
   makeCreateIdeaController,
   makeListIdeasController,
   makeDeleteIdeaController,
+  makeUpdateIdeaController,
 } from '@/main/factories/controllers';
 
 export default (router: Router): void => {
   router.post('/idea', auth, adaptRoute(makeCreateIdeaController()));
   router.get('/ideas', auth, adaptRoute(makeListIdeasController()));
   router.delete('/idea/:ideaId', auth, validateIdeaId, adaptRoute(makeDeleteIdeaController()));
+  router.put('/idea/:ideaId', auth, validateIdeaId, adaptRoute(makeUpdateIdeaController()));
 };
