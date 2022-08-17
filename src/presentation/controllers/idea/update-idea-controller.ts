78b8f24098855782b13b508ba37a99e627de6a68
Allysson <allyssonmgs@gmail.com>
2022-08-16 21:22:02 -0300

    feat: ensure UpdateIdeaController returns 400 if Validation returns an error

--- src/presentation/controllers/idea/update-idea-controller.ts
+++ src/presentation/controllers/idea/update-idea-controller.ts
@@ -1,13 +1,16 @@
 import { UpdateIdea } from '@/domain/usecases';
 import { Controller, Validation } from '@/presentation/protocols';
-import { serverError } from '@/presentation/helpers';
+import { badRequest, serverError } from '@/presentation/helpers';
 
 export class UpdateIdeaController implements Controller {
   constructor(private readonly validation: Validation) {}
 
   async handle(request: UpdateIdeaController.Request): Promise<any> {
     try {
-      this.validation.validate(request);
+      const error = this.validation.validate(request);
+      if (error) {
+        return badRequest(error);
+      }
       return Promise.resolve();
     } catch (error) {
       return serverError(error);
