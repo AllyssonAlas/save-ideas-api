330157247fb248165820a116efc9cd01328a69f8
Allysson <allyssonmgs@gmail.com>
2022-08-18 20:33:15 -0300

    feat: ensure DeleteUserUsecase calls DeleteUserRepository with correct values

--- src/data/usecases/user/delete-user-usecase.ts
+++ src/data/usecases/user/delete-user-usecase.ts
@@ -1,10 +1,17 @@
 import { DeleteUser } from '@/domain/usecases';
-import { DeleteIdeasByOwnerIdRepository } from '@/data/protocols/repositories';
+import {
+  DeleteIdeasByOwnerIdRepository,
+  DeleteUserRepository,
+} from '@/data/protocols/repositories';
 
 export class DeleteUserUsecase implements DeleteUser {
-  constructor(private readonly deleteIdeasByOwnerIdRepository: DeleteIdeasByOwnerIdRepository) {}
+  constructor(
+    private readonly deleteIdeasByOwnerIdRepository: DeleteIdeasByOwnerIdRepository,
+    private readonly deleteUserRepository: DeleteUserRepository,
+  ) {}
 
   async perform(params: DeleteUser.Params): Promise<DeleteUser.Result> {
     await this.deleteIdeasByOwnerIdRepository.deleteByOwnerId({ ownerId: params.id });
+    await this.deleteUserRepository.delete(params);
   }
 }
