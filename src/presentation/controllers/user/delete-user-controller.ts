66bc1179a6cf091f7146f5740940fff3a55726eb
Allysson <allyssonmgs@gmail.com>
2022-08-20 19:29:47 -0300

    feat: ensure DeleteUserController returns 500 if DeleteUser throws

--- src/presentation/controllers/user/delete-user-controller.ts
+++ src/presentation/controllers/user/delete-user-controller.ts
@@ -1,11 +1,16 @@
 import { DeleteUser } from '@/domain/usecases';
 import { Controller } from '@/presentation/protocols';
+import { serverError } from '@/presentation/helpers';
 
 export class DeleteUserController implements Controller {
   constructor(private readonly deleteUser: DeleteUser) {}
 
   async handle(request: DeleteUserController.Request): Promise<any> {
-    await this.deleteUser.perform({ id: request.userId });
+    try {
+      await this.deleteUser.perform({ id: request.userId });
+    } catch (error) {
+      return serverError(error);
+    }
   }
 }
 
