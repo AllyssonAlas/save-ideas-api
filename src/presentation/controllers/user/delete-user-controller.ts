2bdc3f0eece74212f21bca0a8453e80fbd9880fb
Allysson <allyssonmgs@gmail.com>
2022-08-20 20:24:29 -0300

    fix: add HttpResponse interface to controller return

--- src/presentation/controllers/user/delete-user-controller.ts
+++ src/presentation/controllers/user/delete-user-controller.ts
@@ -1,11 +1,11 @@
 import { DeleteUser } from '@/domain/usecases';
-import { Controller } from '@/presentation/protocols';
+import { Controller, HttpResponse } from '@/presentation/protocols';
 import { noContent, serverError } from '@/presentation/helpers';
 
 export class DeleteUserController implements Controller {
   constructor(private readonly deleteUser: DeleteUser) {}
 
-  async handle(request: DeleteUserController.Request): Promise<any> {
+  async handle(request: DeleteUserController.Request): Promise<HttpResponse> {
     try {
       await this.deleteUser.perform({ id: request.userId });
       return noContent();
