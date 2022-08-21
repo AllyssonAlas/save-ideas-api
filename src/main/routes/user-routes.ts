ef8abd152d5e2cb03dabd351901401b06e54eba2
Allysson <allyssonmgs@gmail.com>
2022-08-20 21:05:40 -0300

    feat: ensure DELETE /delete-user returns 204

--- src/main/routes/user-routes.ts
+++ src/main/routes/user-routes.ts
@@ -6,10 +6,12 @@ import {
   makeSignUpController,
   makeAuthenticationController,
   makeUpdateUserController,
+  makeDeleteUserController,
 } from '@/main/factories/controllers';
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()));
   router.post('/login', adaptRoute(makeAuthenticationController()));
   router.put('/update-user', auth, adaptRoute(makeUpdateUserController()));
+  router.put('/delete-user', auth, adaptRoute(makeDeleteUserController()));
 };
